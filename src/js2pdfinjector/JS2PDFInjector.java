f9b3ac184ecebe596efc35fb805beaa62048f0cb
Cornerpirate <cornerpirate@gmail.com>
2018-06-22 09:02:20 +0100

    Modified to allow command line usage for scripting

--- src/js2pdfinjector/JS2PDFInjector.java
+++ src/js2pdfinjector/JS2PDFInjector.java
@@ -10,26 +10,51 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.action.type.PDActionJavaScript;
 
 /**
- * This class behaves as follows:
- * 1) File Open Prompt to select PDF file to embed JS into.
- * 2) File Open Prompt to select .js file to embed.
- * 3) Prompt displaying successful execution or error message.
- * 
+ * This class behaves as follows: 1) File Open Prompt to select PDF file to
+ * embed JS into. 2) File Open Prompt to select .js file to embed. 3) Prompt
+ * displaying successful execution or error message.
+ *
  * @author cornerpirate
  */
 public class JS2PDFInjector {
 
+    // These are now command line arguments
+    public static File pdf_in = null;
+    public static File js_in = null;
+
+    /**
+     * To allow this to be scriptable I updated to accept command line args That
+     * means having a usage now which is displayed when no args areprovided.
+     */
+    public static void usage() {
+
+        System.out.println("JS2PDFInjector: Allowing you to easily add JavaScript to a PDF document to execute when PDF opens.");
+        System.out.println("Usage: java -jar JS2PDFInjector.jar <PDF FILE> <JS FILE>");
+        System.out.println("Providing no positional arguments results in a GUI asking for file locations");
+
+    }
+
     public static void main(String args[]) {
 
-        try {
+        // Not checking if files exist or anything. 
+        // Don't run with scissors folks.
+        if (args.length == 2) {
+            pdf_in = new File(args[0]);
+            js_in = new File(args[1]);
 
+        } else {
+            
+            // No args provided. Display usage incase that helps
+            usage() ;
+
+            // User didn't provide args so show them GUI selectors.
             JFileChooser fileChooser = new JFileChooser();
             // do popup to get the file or files for input
             fileChooser.setMultiSelectionEnabled(false);
             FileNameExtensionFilter filter = new FileNameExtensionFilter("PDF Files", "pdf");
             fileChooser.setFileFilter(filter);
 
-            File pdf_in = null;
+            pdf_in = null;
             int returnVal = fileChooser.showOpenDialog(null);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 pdf_in = fileChooser.getSelectedFile();
@@ -38,19 +63,11 @@ public class JS2PDFInjector {
                 System.exit(-1);
             }
 
-            String output_name = pdf_in.getParent() + File.separator + "js_injected_" + pdf_in.getName();
-            System.out.println("output_name: " + output_name);
-            File js_injected_pdf = new File(output_name);
-
-            PDDocument document = PDDocument.load(pdf_in);
-            System.out.println("File Opened: " + pdf_in);
-
             filter = new FileNameExtensionFilter("JavaScript File", "js");
             fileChooser.setFileFilter(filter);
             // clear PDF file from selected.
             fileChooser.setSelectedFile(new File(""));
 
-            File js_in = null;
             returnVal = fileChooser.showOpenDialog(null);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 js_in = fileChooser.getSelectedFile();
@@ -59,9 +76,20 @@ public class JS2PDFInjector {
                 System.exit(-1);
             }
 
+        }
+
+        try {
+
+            String output_name = pdf_in.getParent() + File.separator + "js_injected_" + pdf_in.getName();
+            System.out.println("output_name: " + output_name);
+            File js_injected_pdf = new File(output_name);
+
+            PDDocument document = PDDocument.load(pdf_in);
+            System.out.println("File Opened: " + pdf_in);
+
             System.out.println("File Opened: " + js_in);
 
-            String content = new Scanner(js_in).useDelimiter( File.separator + "Z").next();
+            String content = new Scanner(js_in).useDelimiter(File.separator + "Z").next();
 
             PDActionJavaScript javascript = new PDActionJavaScript(content);
 
@@ -79,4 +107,3 @@ public class JS2PDFInjector {
 
     }
 }
-
